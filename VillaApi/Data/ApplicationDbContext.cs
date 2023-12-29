bf7982eaeea185eb21d3e7085223afa90750a164
iman <dev.imansabet@gmail.com>
2023-12-29 12:57:41 +0330

    DataBase Config

---
+++ VillaApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VillaApi.Models;
+
+namespace VillaApi.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
