ac9da70206fadbe595e310044413f98886a3c2da
iman <dev.imansabet@gmail.com>
2023-12-29 13:32:58 +0330

    Seed Data in DB

--- VillaApi/Data/ApplicationDbContext.cs
+++ VillaApi/Data/ApplicationDbContext.cs
@@ -10,5 +10,37 @@ namespace VillaApi.Data
             
         }
         public DbSet<Villa> Villas { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Villa>().HasData(
+                new Villa() 
+                {
+                    Id= 1,
+                    Name= "Royal Lux" ,
+                    Details = "This a super fancy villa that very good for honeymoon",
+                    ImageUrl = "",
+                    Occupancy = 5,
+                    Rate = 500,
+                    Sqft = 550,
+                    Amenity = "100 23%",
+                    CreatedDate = DateTime.Now,
+                },
+                new Villa()
+                {
+                    Id = 2,
+                    Name = "Jungle Lux",
+                    Details = "This a super fancy villa that is in jungle",
+                    ImageUrl = "",
+                    Occupancy = 6,
+                    Rate = 600,
+                    Sqft = 650,
+                    Amenity = "200 23%",
+                    CreatedDate = DateTime.Now,
+
+                }
+                );
+        }
+
+
     }
 }
---
+++ VillaApi/Migrations/20231229100209_SeedVillaTableWithCreatedDate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace VillaApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedVillaTableWithCreatedDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Amenity", "CreatedDate", "Details", "ImageUrl", "Name", "Occupancy", "Rate", "Sqft", "UpdatedDate" },
+                values: new object[,]
+                {
+                    { 1, "100 23%", new DateTime(2023, 12, 29, 13, 32, 8, 827, DateTimeKind.Local).AddTicks(6601), "This a super fancy villa that very good for honeymoon", "", "Royal Lux", 5, 500.0, 550, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, "200 23%", new DateTime(2023, 12, 29, 13, 32, 8, 827, DateTimeKind.Local).AddTicks(6614), "This a super fancy villa that is in jungle", "", "Jungle Lux", 6, 600.0, 650, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
